17e8ec00c795dbed43bec7c67ae5d49293c2792e
Rae <152399329+Yukirae@users.noreply.github.com>
2023-12-13 17:58:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+let inputName = document.getElementById("prdname");
+let inputPrice = document.getElementById("prdprice")
+const product =  {
+    name:"shoes",
+    price: 92
+    }
+
+function makeItem(title,price,imgURL){
+    return{
+      title inputName.value
+
+    }
+}
